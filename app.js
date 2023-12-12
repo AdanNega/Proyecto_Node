094bcb7ad3e8171585621b4a500573e40e391658
Adan Negrete <adan.negrete2099@hotmail.com>
2023-12-12 13:29:42 -0600

    Mod: app.js Configuring server; remake gitignorefile

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
+// Invocación de express
 const express = require('express');
-
 const app = express();
+
+// Manejo de datos de formulario con urlencoded
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+// Invocacion dotenv
+const dotenv = require('dotenv');
+dotenv.config({path: './env/.env'});
+
+// Directorio Public
+app.use('/resources', express.static('public'));
+app.use('/resources', express.static(__dirname + '/public'));
+
+app.get('/', (req, res)=>{
+    res.send("Holiiii");
+});
 
 app.listen(4000, (req, res)=>{
     console.log('Server Running in https://localhost:4000')
